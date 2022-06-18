79034cfa5a94cbf51e733ce7344f4beab3674a2a
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-06-18 20:41:24 -0300

    feat(Person): completing Person interface

--- src/fhir/Person.ts
+++ src/fhir/Person.ts
@@ -73,9 +73,124 @@ export interface ContactPoint {
   use: ContactPointUse;
 }
 
+export const enum AdministrativeGender {
+  MALE = 'male',
+  FEMALE = 'female',
+  OTHER = 'other',
+  UNKOWN = 'unkown',
+}
+
+export const enum MimeType {
+  JPEG = 'image/jpeg',
+  PNG = 'image/png',
+}
+
+export interface Attachment {
+  contentType: MimeType;
+  url: string;
+}
+
+export const enum AddressUse {
+  HOME = 'home',
+  WORK = 'work',
+  TEMP = 'temp',
+  OLD = 'old',
+  BILLING = 'billing',
+}
+
+export const enum AddressType {
+  POSTAL = 'postal',
+  PHYSICAL = 'physical',
+  BOTH = 'both',
+}
+
+export interface Period {
+  start: Date;
+  end: Date;
+}
+
+export interface Address {
+  use: AddressUse;
+  type: AddressType;
+  line: string;
+  city: string;
+  district: string;
+  state: string;
+  postalCode: string;
+  country: string;
+  period: Period;
+}
+
+export const enum CommonLanguages {
+  ARABIC = 'ar',
+  BENGALI = 'bn',
+  CZECH = 'cs',
+  DANISH = 'da',
+  GERMAN = 'de',
+  GERMAN_AUSTRIA = 'de-AT',
+  GERMAN_SWITZERLAND = 'de-CH',
+  GERMAN_GERMANY = 'de-DE',
+  GREEK = 'el',
+  ENGLISH = 'en',
+  ENGLISH_AUSTRALIA = 'en-AU',
+  ENGLISH_CANADA = 'en-CA',
+  ENGLISH_GREAT_BRITAIN = 'en-GB',
+  ENGLISH_INDIA = 'en-IN',
+  ENGLISH_NEW_ZELAND = 'en-NZ',
+  ENGLISH_SINGAPORE = 'en-SG',
+  ENGLISH_UNITED_STATES = 'en-US',
+  SPANISH = 'es',
+  SPANISH_ARGENTINA = 'es-AR',
+  SPANISH_SPAIN = 'es-ES',
+  SPANISH_URUGUAY = 'es-UY',
+  FINNISH = 'fi',
+  FRENCH = 'fr',
+  FRENCH_BELGIUM = 'fr-BE',
+  FRENCH_SWITZERLAND = 'fr-CH',
+  FRENCH_FRANCE = 'fr-FR',
+  FRYSIAN = 'fy',
+  FRYSIAN_NETHERLANDS = 'fy-NL',
+  HINDI = 'hi',
+  CROATIAN = 'hr',
+  ITALIAN = 'it',
+  ITALIAN_SWITZERLAND = 'it-CH',
+  ITALIAN_ITALY = 'it-IT',
+  JAPANESE = 'ja',
+  KOREAN = 'ko',
+  DUTCH = 'nl',
+  DUTCH_BELGIUM = 'nl-BE',
+  DUTCH_NETHERLANDS = 'nl-NL',
+  NORWEGIAN = 'no',
+  NORWEGIAN_NORWAY = 'no-NO',
+  PUNJABI = 'pa',
+  POLISH = 'pl',
+  PORTUGUESE = 'pt',
+  PORTUGUESE_BRAZIL = 'pt-BR',
+  RUSSIAN = 'ru',
+  RUSSIAN_RUSSIA = 'ru-RU',
+  SERBIAN = 'sr',
+  SERBIAN_SERBIA = 'sr-RS',
+  SWEDISH = 'sv',
+  SWEDISH_SWEDEN = 'sv-SE',
+  TELEGU = 'te',
+  CHINESE = 'zh',
+  CHINESE_CHINA = 'zh-CN',
+  CHINESE_HONG_KONG = 'zh-HK',
+  CHINESE_SINGAPORE = 'zh-SG',
+  CHINESE_TAIWAN = 'zh-TW',
+}
+
+export interface Communication {
+  language: CommonLanguages;
+}
+
 export interface Person {
   identifier: Identifier;
   active: boolean;
   name: HumanName;
   telecom: ContactPoint;
+  gender: AdministrativeGender;
+  birthDate: Date;
+  photo: Attachment;
+  communication: Communication;
 }
