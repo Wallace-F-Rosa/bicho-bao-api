2ea257159c2c8422878dcf564f2334665908a71a
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-06-21 20:48:46 -0300

    feat(user): create user dto fields

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,9 @@
-export class CreateUserDto {}
+import { Person } from '@src/fhir/Person';
+
+export class CreateUserDto {
+  username: string;
+  passwordHash: string;
+  email: string;
+  roles: string[];
+  personalData: Person;
+}
