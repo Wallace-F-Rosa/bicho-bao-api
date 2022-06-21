debed26c4ccf7eb6684f67c5e117c9419b7c45fb
Wallace Ferancini Rosa <wferancini@gmail.com>
2022-06-20 21:09:16 -0300

    test(user): create owner user

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,4 +1,17 @@
+import { faker } from '@faker-js/faker';
 import { Test, TestingModule } from '@nestjs/testing';
+import {
+  AdministrativeGender,
+  CommonLanguages,
+  ContactPointSystem,
+  ContactPointUse,
+  IdentifierType,
+  IdentifierUse,
+  MimeType,
+  NameUse,
+  Person,
+} from '@fhir/Person';
+import { CreateUserDto } from './dto/create-user.dto';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
@@ -16,5 +29,48 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create user', () => {
+    describe('valid user', () => {
+      it('owner', async () => {
+        const personalData: Person = {
+          identifier: {
+            use: IdentifierUse.OFFICIAL,
+            type: IdentifierType.TAX_ID,
+            value: '844.977.040-84',
+          },
+          active: true,
+          name: {
+            use: NameUse.OFFICIAL,
+            text: faker.name.findName(),
+          },
+          telecom: {
+            system: ContactPointSystem.PHONE,
+            value: faker.phone
+              .phoneNumber('+## ### 9#### ####')
+              .replace(/\s/g, ''),
+            use: ContactPointUse.HOME,
+          },
+          gender: AdministrativeGender.OTHER,
+          birthDate: new Date(),
+          photo: {
+            contentType: MimeType.JPEG,
+            url: faker.internet.avatar(),
+          },
+          communication: {
+            language: CommonLanguages.PORTUGUESE_BRAZIL,
+          },
+        };
+        const userData: CreateUserDto = {
+          username: faker.internet.userName(),
+          password: faker.internet.password(),
+          email: faker.internet.email(),
+          roles: ['admin'],
+          personalData,
+        };
+        expect(controller.create(userData)).toMatchObject(userData);
+      });
+    });
   });
 });
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): User {
     return this.userService.create(createUserDto);
   }
 
